43805cd6fef75c860cfe60643c6ee5adc7661c76
Daniella <daniellamarialer@outlook.com>
2023-07-27 16:25:06 -0300

    Aula de variáveis

---
+++ trilha-ifood-back-end/javascript/variaveis.js
@@ -0,0 +1,31 @@
+//VARIÁVEIS
+
+let idade = 26 //Criando a primeira variável 
+
+console.log(idade)
+
+//Tipos de variáveis
+
+//Mudando o valor da variável já declarada
+              
+idade = 30               // Number 
+let numero = 30         // Number
+let altura = 1.77      //Number
+let nome = 'Daniella' //String  //Variáves do tipo string sempre entre aspas
+let estudando = true  //Booleano
+
+console.log(numero, typeof numero)
+console.log(altura, typeof altura)
+console.log(nome, typeof nome)
+console.log(estudando, typeof estudando)
+
+let outra //undefined
+console.log(outra)
+
+let nula = null;  //null
+console.log(nula)
+
+var variavel = 30 //var sintaxe mais antiga, não utilizar 
+
+const constante = 100 //constante, não muda valor 
+
