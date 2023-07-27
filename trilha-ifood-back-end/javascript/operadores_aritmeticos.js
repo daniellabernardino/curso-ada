e3753792e93d917a66fd7275017b599f1e95f64c
Daniella <daniellamarialer@outlook.com>
2023-07-27 17:01:08 -0300

    Aula de operadores aritméticos

---
+++ trilha-ifood-back-end/javascript/operadores_aritmeticos.js
@@ -0,0 +1,32 @@
+//OPERADORES ARITMÉTICOS
+
+let primeiroNumero = 20
+let segundoNumero = 2
+
+/*
+  Adição:           +
+  Subtração:        - 
+  Multiplicação:    *
+  Divisão:          /
+  Exponenciação:    **
+  Resto da divisão: %
+
+*/
+const soma = primeiroNumero + segundoNumero 
+
+console.log("Soma = ", soma)
+console.log("Subtração = ", primeiroNumero - segundoNumero )
+console.log("Multiplicação = ", primeiroNumero * segundoNumero )
+console.log("Divisão = ", primeiroNumero / segundoNumero )
+console.log("Exponenciação = ", primeiroNumero ** segundoNumero )
+console.log("Resto de divisão = ", primeiroNumero % segundoNumero )
+
+
+//Precedência de operadores
+
+console.log(3 + 7 * 3)
+
+//Biblioteca Math
+
+console.log(Math.PI)
+console.log(Math.sqrt(16)) // Calcula raiz quadrada de um número 
