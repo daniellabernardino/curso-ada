07cc245e1909f9230f488d02fd12e182cd3edaa5
Daniella <daniellamarialer@outlook.com>
2023-07-31 16:48:47 -0300

    Aula de estruturas condicionais

---
+++ trilha-ifood-back-end/javascript/estruturas_condicionais.js
@@ -0,0 +1,26 @@
+// ESTRUTURAS CONDICIONAIS 
+
+const idade = 20
+
+if (idade >= 18){
+  console.log("Você é maior de idade!")
+  
+}else{
+  console.log("Você é menor de idade!")  
+}
+
+// Se media >= 7, aprovado 
+//Se media < 7 e media >= 5, recuperação
+//Se media < 5, reprovado
+
+console.clear()
+
+let media = 10
+
+if (media >= 7) {
+  console.log("Aprovada(o)!")
+} else if (media >= 5 ) {
+  console.log("Recumeração!")  
+} else {
+  console.log("Reprovada(o)!")   
+}
