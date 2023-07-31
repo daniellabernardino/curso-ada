aaf06520c3ca7661f0314552463ecd6b12d841b6
Daniella <daniellamarialer@outlook.com>
2023-07-31 17:08:35 -0300

    Aula de laços condicionais

--- trilha-ifood-back-end/javascript/lacos_condicionais.js
+++ trilha-ifood-back-end/javascript/lacos_condicionais.js
@@ -1,4 +1,14 @@
 //LAÇOS CONDICIONAIS
 
+const input = require("readline-sync") // importando biblioteca 
+
 const numeroSorteado = 5 
+
+let numero = Number( input.question("Qual numero voce escolhe? ") )  //Converte o que o usuário digitou pra número
+
+while (numero !== numeroSorteado) {
+  console.log("Você errou!Tente novamente...")
+  numero = Number( input.question("Qual numero voce escolhe? ") )  
+}
+console.log("Você acertou!")
 
