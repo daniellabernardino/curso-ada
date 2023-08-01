0c3f7a4521835e146ec34c6ae016014befa6a108
Daniella <daniellamarialer@outlook.com>
2023-08-01 12:41:59 -0300

    Começando módulo POO em java, aula Hello world com java

---
+++ introducao_a_poo_em_java/main.java
@@ -0,0 +1,17 @@
+
+public class main {
+  public static void main(String[] args) {
+
+    System.out.println("Hello World!");
+    byte variavelByte = -127;
+    //short -32768 a 32767
+    //char  0 a 65535
+    //int -2bi a 2bi
+    //long -9tri a 9tri
+
+    int variavelInt = 2147855;
+
+    System.out.println(variavelInt);
+
+  }
+}
